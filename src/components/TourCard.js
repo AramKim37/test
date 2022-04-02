d56fef8a0d1f06877b811eec5a37674065fa54e0
aram kim <aramkim@MacBook-Pro-de-aram.local>
2022-04-01 22:34:20 -0600

    test revert

---
+++ src/components/TourCard.js
@@ -0,0 +1,14 @@
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img src="" alt="" />
+      </Paper>
+    </Grid>
+  );
+};
+
+export default TourCard;
